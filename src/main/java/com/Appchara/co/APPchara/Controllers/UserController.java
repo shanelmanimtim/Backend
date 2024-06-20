b2d904607563935c29fec439c084df9a9000c98c
shanelmanimtim <21-12973@g.batstate-u.edu.ph>
2024-06-20 15:33:47 +0800

    configure to database

--- src/main/java/com/Appchara/co/APPchara/Controllers/UserController.java
+++ src/main/java/com/Appchara/co/APPchara/Controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.Appchara.co.APPchara.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Appchara.co.APPchara.Model.UserModel;
@@ -11,5 +15,22 @@ public class UserController {
     @GetMapping("/User")
     public UserModel getUser(){
         return new UserModel(1, "jeven 11", "jeven.11@email.com", "jeven11");
+    }
+
+    //http://localhost:8080/Users
+    @GetMapping("/Users")
+    public List <UserModel> getUsers(){
+        List<UserModel> users = new ArrayList<>();
+        users.add(new UserModel(1, "mario", "mario@email.com", "123456789"));
+        users.add(new UserModel(2, "juan", "juan@email.com", "1234567810"));
+        users.add(new UserModel(3, "pedro", "pedro@email.com", "1234567890"));
+        return users;
+
+    }
+
+    //http:localhost:8080/users/jopay
+    @GetMapping("/users/{name}")
+    public UserModel getUserFromName(@PathVariable("name")String name){
+        return new UserModel(1, "jopay", "jopay@email.com","wagkangmawala");
     }
 }
