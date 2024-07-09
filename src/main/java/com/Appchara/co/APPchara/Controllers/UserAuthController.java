80d0e81cab9b85285cd07dd70dff271622e4d55c
Trixieee20 <trixieemacalalad@gmail.com>
2024-07-09 15:12:25 +0800

    Login/Register

--- src/main/java/com/Appchara/co/APPchara/Controllers/UserAuthController.java
+++ src/main/java/com/Appchara/co/APPchara/Controllers/UserAuthController.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Appchara.co.APPchara.DTO.LoginRequest;
 import com.Appchara.co.APPchara.DTO.RegistrationRequest;
 import com.Appchara.co.APPchara.Model.Role;
 import com.Appchara.co.APPchara.Model.UserAuth;
@@ -19,8 +23,8 @@ import com.Appchara.co.APPchara.Repository.RoleRepository;
 import com.Appchara.co.APPchara.Repository.UserAuthRepository;
 
 @RestController
-@RequestMapping("/api/v1/auth")
-public class AuthController {
+@RequestMapping("/api/v1/auth/")
+public class UserAuthController {
 
     @Autowired
     UserAuthRepository userAuthRepository;
@@ -59,4 +63,45 @@ public class AuthController {
         return new ResponseEntity<>("User registered sucessfully", HttpStatus.OK);
     }
 
+    @PostMapping("/register/user")
+    public ResponseEntity<?> registeruser(@RequestBody RegistrationRequest registrationRequest){
+
+        if(userAuthRepository.existsByUsername(registrationRequest.getUsername())){
+            return new ResponseEntity<>("Username is already taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        if(userAuthRepository.existsByEmail(registrationRequest.getEmail())){
+            return new ResponseEntity<>("An account is already registered in this", HttpStatus.BAD_REQUEST);
+        }
+
+        UserAuth userAuth = new UserAuth(
+            registrationRequest.getUsername(),
+            registrationRequest.getEmail(),
+            passwordEncoder.encode(registrationRequest.getPassword())
+        );
+
+        Role role = roleRepository.findByName("ROLE_USER").get();
+        userAuth.setRoles(Collections.singleton(role));
+
+        userAuthRepository.save(userAuth);
+
+        return new ResponseEntity<>("User registered sucessfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
+        try{
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    loginRequest.getUsernameOrEmail(),
+                    loginRequest.getPassword()
+                )
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return new ResponseEntity<>("User logged in successfully", HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
+        }
+    }
 }
